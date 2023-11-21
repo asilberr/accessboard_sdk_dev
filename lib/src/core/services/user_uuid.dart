03f36d81f20c2f60dcebe104da71c23dbfdfc347
asilberr <a.silberreiss@hotmail.com>
2023-11-21 12:34:48 +0100

    ..

--- lib/src/core/services/user_uuid.dart
+++ lib/src/core/services/user_uuid.dart
@@ -1,13 +1,24 @@
+// user_uuid.dart
+
 import 'package:uuid/uuid.dart';
 import 'package:device_info/device_info.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserUUID {
   String _deviceIdentifier = '';
   String _customIdentifier = '';
+  String _cachedUUID = '';
 
-  // Constructor to initialize device identifier
-  UserUUID() {
+  // Singleton pattern for the UserUUID class
+  static final UserUUID _instance = UserUUID._internal();
+
+  factory UserUUID() {
+    return _instance;
+  }
+
+  UserUUID._internal() {
     _initDeviceIdentifier();
+    _loadCachedUUID();
   }
 
   // Initialize the device identifier using the device_info package
@@ -17,18 +28,34 @@ class UserUUID {
     _deviceIdentifier = androidInfo.androidId;
   }
 
+  // Load cached UUID from SharedPreferences
+  Future<void> _loadCachedUUID() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _cachedUUID = prefs.getString('userUUID') ?? '';
+  }
+
   // Set custom identifier for the user
   void setCustomIdentifier(String customIdentifier) {
     _customIdentifier = customIdentifier;
+    _generateUUID(); // Regenerate UUID when custom identifier changes
   }
 
-  // Generate the user UUID
-  String generateUUID() {
+  // Generate and cache the user UUID
+  void _generateUUID() {
     // Concatenate device identifier and custom identifier
     String combinedIdentifier = '$_deviceIdentifier:$_customIdentifier';
 
     // Generate a UUID using a simple hash function
-    return _simpleHash(combinedIdentifier);
+    _cachedUUID = _simpleHash(combinedIdentifier);
+
+    // Save the UUID to SharedPreferences for persistence
+    _saveUUIDToPrefs();
+  }
+
+  // Save the UUID to SharedPreferences
+  Future<void> _saveUUIDToPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('userUUID', _cachedUUID);
   }
 
   // Simple hash function to generate a UUID-like string
@@ -39,16 +66,9 @@ class UserUUID {
     }
     return Uuid().v5(Uuid.NAMESPACE_URL, hash.toString());
   }
-}
 
-void main() async {
-  // Create an instance of UserUUID
-  UserUUID userUUID = UserUUID();
-
-  // Set custom identifier (replace with your user's unique identifier)
-  userUUID.setCustomIdentifier('john_doe');
-
-  // Generate and print the user UUID
-  String uuid = userUUID.generateUUID();
-  print('User UUID: $uuid');
+  // Get the current user UUID
+  String getUserUUID() {
+    return _cachedUUID;
+  }
 }
